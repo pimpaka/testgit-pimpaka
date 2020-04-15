6f8b9b61c6efbbc2a423232bbdd27d11d8578a2b
pimpaka stewart <35515307+pimpaka@users.noreply.github.com>
2020-04-15 21:38:50 +0700

    Updates

--- precache-manifest.7041376f9ca104a02346c6396ae5aeb1.js
+++ precache-manifest.7041376f9ca104a02346c6396ae5aeb1.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c9e1284ebfd8f0e67ca930e8abedd30d",
+    "revision": "f71c45208bab6008ae42286540c0187f",
     "url": "/testgit-pimpaka/index.html"
   },
   {
-    "revision": "3320bf2ed0f18fe66cb8",
+    "revision": "a54762eb38e9ba543791",
     "url": "/testgit-pimpaka/static/css/main.09b4a74a.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/testgit-pimpaka/static/js/2.7ed52910.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3320bf2ed0f18fe66cb8",
-    "url": "/testgit-pimpaka/static/js/main.cd7e95ea.chunk.js"
+    "revision": "a54762eb38e9ba543791",
+    "url": "/testgit-pimpaka/static/js/main.facda60d.chunk.js"
   },
   {
     "revision": "c161466dc97cbcc216e7",
