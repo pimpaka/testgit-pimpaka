21f64d309333c67a9d275c146a3c40423942ae36
pimpaka stewart <35515307+pimpaka@users.noreply.github.com>
2020-04-15 21:12:57 +0700

    .

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/testgit-pimpaka/precache-manifest.96858f0b71a668953d89c2898146624a.js"
+  "/testgit-pimpaka/precache-manifest.94c96b30514fa05ab21745ceeb597a4c.js"
 );
 
 self.addEventListener('message', (event) => {
--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ class App extends Component {
   }
 
   componentDidMount () {
-    fetch('https://cors-anywhere.herokuapp.com/https://jsonplaceholder.typicode.com/users')
+    fetch('https://jsonplaceholder.typicode.com/users')
+    
     .then(response => response.json())
     .then(user => this.setState({ monsters: user }))
   }
